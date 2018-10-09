4100cb0faa68bab537ca605ddb5a8141bc3d6c82
BT_1N3_13 <tlee3526@bths.edu>
2018-10-09 10:13:47 -0400

    Hi

---
+++ src/Teacher.java
@@ -0,0 +1,17 @@
+public class Teacher extends Runner.Person {
+private String subject, title;
+    public Teacher(String subject, String title)
+    {
+        super ("teacher");
+        this.subject = subject;
+        this.title = title;
+    }
+    public void getSubject main(String subject)
+    {
+        return this.subject;
+    }
+    public void title main(String title)
+    {
+        return this.title;
+    }
+}
